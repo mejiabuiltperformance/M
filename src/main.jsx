db457f9809e1378cadb39ee6b8e313e26f80611f
mejiabuiltperformance <julianmejia831@gmail.com>
2026-05-25 23:47:34 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
+import hero from './C1A60692-FC46-4E2C-840E-C05496E3D9FC.png'
 
 function App() {
   return (
-    <div style={{
-      background:'black',
-      color:'white',
-      height:'100vh',
-      display:'flex',
-      justifyContent:'center',
-      alignItems:'center',
-      fontSize:'40px'
-    }}>
-      MEJIA BUILT PERFORMANCE
-    </div>
+    <main>
+      <img className="heroImage" src={hero} alt="Mejia Built Performance" />
+    </main>
   )
 }
 
